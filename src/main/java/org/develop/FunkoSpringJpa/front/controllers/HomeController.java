198c87a5a5428d8a61c7ccab8b2d89bdf0178ed9
Alonso2002-jpg <jorgealonso0876@gmail.com>
2023-11-20 19:36:24 +0100

    Implementando cambios en Paginas

--- src/main/java/org/develop/FunkoSpringJpa/front/controllers/HomeController.java
+++ src/main/java/org/develop/FunkoSpringJpa/front/controllers/HomeController.java
@@ -3,6 +3,7 @@ package org.develop.FunkoSpringJpa.front.controllers;
 import org.develop.FunkoSpringJpa.rest.categorias.commons.model.Categoria;
 import org.develop.FunkoSpringJpa.rest.categorias.repositories.CategoriaRepository;
 import org.develop.FunkoSpringJpa.rest.funko.commons.mainUse.model.Funko;
+import org.develop.FunkoSpringJpa.rest.funko.exceptions.FunkoNotFound;
 import org.develop.FunkoSpringJpa.rest.funko.repositories.FunkoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -30,11 +31,40 @@ public class HomeController {
         return "index";
     }
 
-    @GetMapping("/{category}")
+    @GetMapping("/funkos/{category}")
     public String listFunkosSeccion(Model model, @PathVariable String category){
         Categoria cateFound = categoriaRepository.findByNameCategoryIgnoreCase(category).get();
         List<Funko> funkos = funkoRepository.findAllByCategory(cateFound);
         model.addAttribute("funkos", funkos);
         return "blank";
+    }
+
+    @GetMapping("/funkos/create")
+    public String createFunko(Model model){
+        model.addAttribute("funko", new Funko());
+        return "form-post";
+    }
+
+    @GetMapping("/funkos/update/{id}")
+    public String updateFunko(Model model, @PathVariable Long id){
+        Funko funko = funkoRepository.findById(id).orElseThrow(()-> new FunkoNotFound(id));
+        List<Categoria> categorias = categoriaRepository.findAll();
+        model.addAttribute("funko", funko);
+        model.addAttribute("categorias",categorias);
+        return "form-put";
+    }
+
+    @GetMapping("/funkos/updateImg")
+    public String updateImgFunko(Model model, @PathVariable Long id){
+        Funko funko = funkoRepository.findById(id).orElseThrow(()-> new FunkoNotFound(id));
+        model.addAttribute("funko", funko);
+        return "form-update-img";
+    }
+
+    @GetMapping("/funkos/delete")
+    public String deleteFunko(Model model){
+        List<Funko> funkos = funkoRepository.findAll();
+        model.addAttribute("funkos", funkos);
+        return "tables";
     }
 }
