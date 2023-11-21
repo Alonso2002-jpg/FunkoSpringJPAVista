c2a34b93cf39aaf59fbf6f9e826d4ed827e45588
Alonso2002-jpg <jorgealonso0876@gmail.com>
2023-11-21 13:34:55 +0100

    Cambios en la vista

--- src/main/java/org/develop/FunkoSpringJpa/front/controllers/HomeController.java
+++ src/main/java/org/develop/FunkoSpringJpa/front/controllers/HomeController.java
@@ -1,70 +1,119 @@
 package org.develop.FunkoSpringJpa.front.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.develop.FunkoSpringJpa.rest.categorias.commons.model.Categoria;
 import org.develop.FunkoSpringJpa.rest.categorias.repositories.CategoriaRepository;
+import org.develop.FunkoSpringJpa.rest.funko.commons.dto.FunkoCreateDto;
+import org.develop.FunkoSpringJpa.rest.funko.commons.dto.FunkoUpdateDto;
 import org.develop.FunkoSpringJpa.rest.funko.commons.mainUse.model.Funko;
 import org.develop.FunkoSpringJpa.rest.funko.exceptions.FunkoNotFound;
 import org.develop.FunkoSpringJpa.rest.funko.repositories.FunkoRepository;
+import org.develop.FunkoSpringJpa.rest.funko.services.FunkoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
+@Slf4j
 public class HomeController {
-    private final FunkoRepository funkoRepository;
+    private final FunkoService funkoService;
     private final CategoriaRepository categoriaRepository;
 
     @Autowired
-    public HomeController(FunkoRepository funkoRepository, CategoriaRepository categoriaRepository) {
-        this.funkoRepository = funkoRepository;
+    public HomeController(FunkoService funkoService, CategoriaRepository categoriaRepository) {
+        this.funkoService = funkoService;
         this.categoriaRepository = categoriaRepository;
     }
     @GetMapping("/")
-    public String listFunkos(Model model) {
-        List<Funko> funkos = funkoRepository.findAll();
-        model.addAttribute("funkos",funkos);
+    public String listFunkos(Model model,
+                        @RequestParam(value = "name") Optional<String> name,
+                        @RequestParam(value = "quantity") Optional<Integer> quantity,
+                        @RequestParam(value = "price") Optional<Double> price,
+                        @RequestParam(value = "category") Optional<String> category,
+                        @RequestParam(defaultValue = "0") int page,
+                        @RequestParam(defaultValue = "4") int size,
+                        @RequestParam(defaultValue = "id") String sortBy,
+                        @RequestParam(defaultValue = "asc") String direction) {
+        log.info(name.orElse(""));
+        Sort sort = direction.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortBy).ascending() : Sort.by(sortBy).descending();
+        Pageable pageable = PageRequest.of(page, size, sort);
+        Page<Funko> funkosPage = funkoService.getAll(name, quantity,price, category, pageable);
+        model.addAttribute("name", name.orElse(""));
+        model.addAttribute("funkosPage",funkosPage);
         return "index";
     }
 
-    @GetMapping("/funkos/{category}")
-    public String listFunkosSeccion(Model model, @PathVariable String category){
-        Categoria cateFound = categoriaRepository.findByNameCategoryIgnoreCase(category).get();
-        List<Funko> funkos = funkoRepository.findAllByCategory(cateFound);
-        model.addAttribute("funkos", funkos);
-        return "blank";
-    }
-
     @GetMapping("/funkos/create")
-    public String createFunko(Model model){
-        model.addAttribute("funko", new Funko());
+    public String createFunkoForm(Model model){
+        List<Categoria> categorias = categoriaRepository.findAll();
+        model.addAttribute("categorias", categorias);
+        model.addAttribute("funko", FunkoCreateDto.builder().build());
         return "form-post";
     }
 
+    @PostMapping("/funkos/create")
+    public String createFunko(@ModelAttribute("funko") FunkoCreateDto funko){
+        System.out.println(funko);
+        funkoService.save(funko);
+        return "redirect:/";
+    }
+
     @GetMapping("/funkos/update/{id}")
-    public String updateFunko(Model model, @PathVariable Long id){
-        Funko funko = funkoRepository.findById(id).orElseThrow(()-> new FunkoNotFound(id));
+    public String editFunko(Model model, @PathVariable Long id){
+        Funko funko = funkoService.findById(id);
         List<Categoria> categorias = categoriaRepository.findAll();
         model.addAttribute("funko", funko);
         model.addAttribute("categorias",categorias);
         return "form-put";
     }
 
-    @GetMapping("/funkos/updateImg")
-    public String updateImgFunko(Model model, @PathVariable Long id){
-        Funko funko = funkoRepository.findById(id).orElseThrow(()-> new FunkoNotFound(id));
-        model.addAttribute("funko", funko);
-        return "form-update-img";
+    @PostMapping("/funkos/update/{id}")
+    public String updateFunko(@PathVariable Long id, @ModelAttribute("funko") FunkoUpdateDto funko){
+        funkoService.update(id,funko);
+        return "redirect:/funkos/gestion";
     }
 
-    @GetMapping("/funkos/delete")
-    public String deleteFunko(Model model){
-        List<Funko> funkos = funkoRepository.findAll();
-        model.addAttribute("funkos", funkos);
+    @GetMapping("/funkos/updateImg/{id}")
+    public String updateImgFunko(Model model, @PathVariable Long id){
+        Funko funko = funkoService.findById(id);
+        List<Categoria> categorias = categoriaRepository.findAll();
+        model.addAttribute("funko", funko);
+        model.addAttribute("categorias",categorias);
+        return "form-put-img";
+    }
+
+    @PostMapping("/funkos/updateImg/{id}")
+    public String updatePathImgFunko(@PathVariable Long id,@RequestParam("file") MultipartFile file){
+        funkoService.updateImage(id, file);
+        return "redirect:/funkos/gestion";
+    }
+
+    @GetMapping("/funkos/gestion")
+    public String gestionFunkos(Model model,
+                        @RequestParam(defaultValue = "0") int page,
+                        @RequestParam(defaultValue = "5") int size,
+                        @RequestParam(defaultValue = "id") String sortBy,
+                        @RequestParam(defaultValue = "asc") String direction) {
+        Sort sort = direction.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortBy).ascending() : Sort.by(sortBy).descending();
+        Pageable pageable = PageRequest.of(page, size, sort);
+        Page<Funko> funkosPage = funkoService.getAll(Optional.empty(), Optional.empty(),Optional.empty(), Optional.empty(), pageable);
+        model.addAttribute("funkosPage",funkosPage);
         return "tables";
+    }
+
+    @GetMapping("/funkos/delete/{id}")
+    public String deleteFunko(@PathVariable Long id){
+        funkoService.findById(id);
+        funkoService.deleteById(id);
+        return "redirect:/funkos/gestion";
     }
 }
